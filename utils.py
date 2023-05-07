6945d28abfa488da9fb65b4a50f0f31345f98367
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2023-05-07 19:03:22 +0530

    Fixed Everything

--- utils.py
+++ utils.py
@@ -457,7 +457,7 @@ async def get_token(bot, userid, link, fileid):
         await bot.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(user.id, user.mention))
     token = ''.join(random.choices(string.ascii_letters + string.digits, k=7))
     TOKENS[user.id] = {token: False}
-    link = f"{link}verify#{user.id}#{token}#{fileid}"
+    url = f"{link}verify-{user.id}-{token}-{fileid}"
     status = await get_verify_status(user.id)
     date_var = status["date"]
     years, month, day = date_var.split('-')
@@ -471,7 +471,7 @@ async def get_token(bot, userid, link, fileid):
         vr_num = 2
     else:
         vr_num = 1
-    shortened_verify_url = await get_verify_shorted_link(vr_num, link)
+    shortened_verify_url = await get_verify_shorted_link(vr_num, url)
     return str(shortened_verify_url)
 
 async def get_verify_status(userid):
